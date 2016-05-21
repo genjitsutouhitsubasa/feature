b11bd612515eda1969e63f145e841cfba4e45f49
r-hatase <dzm512.myu@gmail.com>
2016-05-21 16:55:15 +0900

    project up

---
+++ TeamGenjitsuTohi/Assets/Script/TABOUnit.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TABOUnit : MonoBehaviour {
+
+
+	private Vector2 frontPos;
+	private Vector2 leftPos;
+	private Vector2 rightPos;
+
+	private int frontId;
+	private int leftId;
+	private int rightId;
+
+
+	public void SetTABOPoint(int fId, int lId, int rId)
+	{
+		this.frontId = fId;
+		this.leftId = lId;
+		this.rightId = rId;
+
+
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.frontPos = Input.GetTouch (this.frontId).position;
+		this.leftPos = Input.GetTouch (this.leftId).position;
+		this.rightPos = Input.GetTouch (this.rightId).position;
+
+	}
+}
---
+++ TeamGenjitsuTohi/Assets/Script/TestManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestManager : MonoBehaviour {
+
+
+	public int touchNum = 0;
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.touchNum = Input.touchCount;
+		foreach (Touch t in Input.touches) {
+			Debug.Log ("id:" + t.fingerId + " pos:" + t.position);
+		}
+
+	}
+}
